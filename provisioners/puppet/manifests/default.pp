570a1ffe14176c0df23f0e466f6d73a89fc39ef9
Pablo Castrillo <acontia@gmail.com>
2012-09-22 20:03:28 +0100

    testing

--- provisioners/puppet/manifests/default.pp
+++ provisioners/puppet/manifests/default.pp
@@ -1,12 +1,12 @@
 # Add a stage which precedes the main installation routine.
-  stage {"pre": before => Stage["main"]}
+stage {"pre": before => Stage["main"]}
 
 # Ensure apt updates before running the main installation routine.
 class {'apt': stage => 'pre'}
 
 # Services used to run Drupal.
-  package { 'apache2': }
-  package { 'mysql-server': }
+class {'apache':  }
+package { 'mysql-server': }
 
 # PHP and a bunch of PHP extensions.
 package { 'php5': }
@@ -26,3 +26,26 @@ phpundeprecate {'/etc/php5/conf.d/mcrypt.ini':
   file => '/etc/php5/conf.d/mcrypt.ini',
   require => Package['php5-mcrypt'],
 }
+
+# Configure Virtual Host                ]
+$site_domain = 'example.com'
+$site_docroot = "/vagrant/${site_domain}/docroot/"
+$site_logroot = "/vagrant/${site_domain}/logroot/"
+
+file { $site_docroot:
+  ensure => directory
+}
+
+file { $site_logroot:
+  ensure => directory
+}
+
+apache::vhost { $site_domain:
+    priority        => '10',
+    vhost_name      => '192.0.2.1',
+    port            => '80',
+    docroot         => $site_docroot,
+    logroot         => $site_logroot,
+    serveradmin     => 'admin@${site_domain}',
+}
+
